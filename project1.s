6dcc14b616e012a8ac33845384668b004129437a
Nicole Sullivan <nicole.sullivan1@bison.howard.edu>
2019-10-28 00:05:54 -0400

    Continuing to update .s file

--- project1.s
+++ project1.s
@@ -17,6 +17,14 @@ main:
 	li $a1, 11 			
 	syscall 
 
+	addi $t0, $zero, 0				#initialize counter for the loop
+	la $s0, str					#load the string into $s0
+	
+	get_char:					#loop for each char of the string
+		beq $t0, 10, exit		#condition to check if it is the end of the string
+		
+		lb $t2, 0($s0)			#loads a character into $t2
+		
 	exit:
 		li $v0, 10 				# exit program
 		syscall
