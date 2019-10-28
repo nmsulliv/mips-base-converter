aaa95ecac7ce1594f90560a5d72eac40849774e8
Nicole Sullivan <nicole.sullivan1@bison.howard.edu>
2019-10-27 23:58:57 -0400

    Updating .s file

--- project1.s
+++ project1.s
@@ -2,6 +2,12 @@
 .data
 
 	prompt: .asciiz "Input: "
+	message: .asciiz "\nOutput: "
 	str: .space 32 # saves space for 32 bytes
 
 .text
+
+main:
+	li $v0, 4 					#prompts the instructions to the user 
+	la $a0, prompt
+	syscall
